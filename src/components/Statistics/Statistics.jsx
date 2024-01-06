6ceac8ab7cfc2ff444c6f65211f0c76ecd83a4b4
GodzilaSanya <godzilasanya@gmail.com>
2024-01-06 13:02:49 +0200

    removed debug log

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,6 @@ import css from './statistics.module.css';
 class Statistics extends Component {
   render() {
     const { good, neutral, bad, total, positivePercentage } = this.props;
-    console.log(total);
     return (
       <>
         <p className={css.result}>
